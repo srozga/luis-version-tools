1a21a20898b7103e5224f63247b7487cfd27e06f
Szymon Rozga <szymon.rozga@gmail.com>
2018-09-10 17:38:29 -0400

    using the right app for import

--- src/updatemodel.js
+++ src/updatemodel.js
@@ -9,6 +9,11 @@ const replaceExt = require('replace-ext');
 const VersionFile = '.luis-app-version';
 const LuisRcFile = '.luisrc';
 
+function npmrunExecSync(cmd) {
+    console.log(chalk.default.white(`exec: ${cmd}`));
+    return npmrun.execSync(cmd);
+}
+
 exports.exportlu = function (program) {
     const appConfig = setupAppConfig(program);
     const tempJson = '___temp.json';
@@ -20,17 +25,17 @@ exports.exportlu = function (program) {
         } else {
             console.log(chalk.default.green('Fetching versions ...'));
             const getVersions = `luis list versions --appId ${appConfig.appId} --authoringKey ${appConfig.authoringKey}`;
-            const getVersionsResult = JSON.parse(npmrun.execSync(getVersions));
+            const getVersionsResult = JSON.parse(npmrunExecSync(getVersions));
             latestVersion = getVersionsResult[getVersionsResult.length - 1].version;
         }
 
         console.log(chalk.default.green(`Exporting version ${latestVersion} ...`));
         const exportCmd = `luis export version --appId ${appConfig.appId} --authoringKey ${appConfig.authoringKey} --versionId ${latestVersion} > ${tempJson}`;
-        const exportCmdResult = npmrun.execSync(exportCmd);
+        const exportCmdResult = npmrunExecSync(exportCmd);
 
         console.log(chalk.default.green(`Refreshing .lu ...`));
         const ludownCmd = `ludown refresh -i ${tempJson} -n ${program.model} --skip_header`;
-        const ludownCmdResult = npmrun.execSync(ludownCmd);
+        const ludownCmdResult = npmrunExecSync(ludownCmd);
 
         console.log(chalk.default.green(`Done! Created ${program.model} ...`));
 
@@ -57,7 +62,7 @@ exports.updateModel = function (program) {
 
     console.log(chalk.default.green(`Getting app id ${appConfig.appId}...`));
     const appCmd = `luis get application --appId ${appConfig.appId} --authoringKey ${appConfig.authoringKey}`;
-    const appCmdResult = npmrun.execSync(appCmd);
+    const appCmdResult = npmrunExecSync(appCmd);
     if (appCmdResult.indexOf('is not a valid value for appId') >= 0) {
         console.log(chalk.default.redBright(`Application ${appConfig.appId} not found.`));
         process.exit();
@@ -93,7 +98,7 @@ exports.updateModel = function (program) {
 
     let versionExists = false;
     try {
-        getVersionResult = npmrun.execSync(getVersion, { stdio: 'ignore' });
+        getVersionResult = npmrunExecSync(getVersion, { stdio: 'ignore' });
         console.log(chalk.default.yellowBright(`Version ${version} exists...`));
         versionExists = true;
     } catch (error) {
@@ -109,7 +114,7 @@ exports.updateModel = function (program) {
             ludownResult = runLudown(program, version, appCmdResultJson);
         }
         if (!versionExists) {
-            importVersion(program, version, ludownResult);
+            importVersion(program, appConfig, version, ludownResult);
         } else {
             // if (program.force) {
             //     console.log(chalk.default.yellowBright('Forcing...'));
@@ -132,7 +137,7 @@ exports.updateModel = function (program) {
 
             console.log(chalk.default.green('Publishing...'));
             const publishCmd = `luis publish version --appId ${appConfig.appId} --versionId ${version} --authoringKey ${appConfig.authoringKey} --endpointBasePath ${appConfig.endpointBasePath}`;
-            const publishResult = JSON.parse(npmrun.execSync(publishCmd));
+            const publishResult = JSON.parse(npmrunExecSync(publishCmd));
         } else {
             console.log(chalk.default.yellowBright('Skipping publish. Use --publish option to include publish.'));
         }
@@ -151,7 +156,7 @@ function runLudown(program, version, applicationJson) {
     console.log(chalk.default.green('Running ludown.'));
     const n = applicationJson.name;
     const ludownCmd = `ludown parse ToLuis --in ${program.model} --luis_name ${n} --luis_desc "${applicationJson.description}" --luis_versionId ${version} --luis_culture ${applicationJson.culture}`;
-    const ludownCmdResult = npmrun.execSync(ludownCmd);
+    const ludownCmdResult = npmrunExecSync(ludownCmd);
     if (ludownCmdResult.toString().length > 0) {
         // there was an issue with ludown
         throw new Error('Issue with ludown: ' + ludownCmdResult);
@@ -168,7 +173,7 @@ function runLudown(program, version, applicationJson) {
 function deleteVersion(program, version, appConfig) {
     console.log(chalk.default.green('Backing version up before delete'));
     const backupVersionCmd = `luis export version --versionId ${version} --appId ${appConfig.appId} --authoringKey ${appConfig.authoringKey}`;
-    const backupVersionCmdResult = npmrun.execSync(backupVersionCmd);
+    const backupVersionCmdResult = npmrunExecSync(backupVersionCmd);
 
     const deleteVersionCmd = `luis delete version --versionId ${version} --appId ${appConfig.appId} --authoringKey ${appConfig.authoringKey}`;
     const deleteVersionCmdProcess = npmrun.exec(deleteVersionCmd, {}, function (err, stdout, stderr) {
@@ -179,10 +184,10 @@ function deleteVersion(program, version, appConfig) {
     console.log(chalk.default.green('Version deleted.'));
 }
 
-function importVersion(program, version, jsonFilename) {
+function importVersion(program, appConfig, version, jsonFilename) {
     console.log(chalk.green(`Importing version ${version}...`));
-    const updateCmd = `luis import version --in ${jsonFilename}`;
-    const updateCmdResult = npmrun.execSync(updateCmd);
+    const updateCmd = `luis import version --appId ${appConfig.appId} --authoringKey ${appConfig.authoringKey} --in ${jsonFilename}`;
+    const updateCmdResult = npmrunExecSync(updateCmd);
 
     fs.unlinkSync(jsonFilename); // get rid of the old file
 
@@ -199,13 +204,13 @@ function trainVersion(version, appConfig) {
         if (retryCount > 0) {
             console.log(chalk.default.yellow(`Retry ${retryCount}: ${trainVersion}`));
         }
-        npmrun.execSync(trainVersion);
+        npmrunExecSync(trainVersion);
         utils.waitfor(5);
         var start = moment();
         while (true) {
             const getStatus = `luis get status --appId ${appConfig.appId} --versionId ${version} --authoringKey ${appConfig.authoringKey}`;
 
-            const statusStr = npmrun.execSync(getStatus).toString();
+            const statusStr = npmrunExecSync(getStatus).toString();
 
             let isDone = null;
             let needsRetrain = null;
